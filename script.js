4891c08d817fdd81fefa976a91edd45fe0e69b03
Temi Michael <olasehindemichael285@gmail.com>
2024-09-26 09:44:15 +0100

    Style corrections

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let profileName = document.querySelector(".name");
 let labName = document.querySelector(".labName");
-profileName.textContent = 'Isaac Odinaka';
-labName.textContent = profileName.textContent + "'s" + " Laboratory"
+const underlineElements = document.querySelectorAll('.underline');
 
+profileName.textContent = 'Isaac Odinaka';
+labName.textContent = profileName.textContent + "'s" + " Laboratory";
+
+let elements = document.querySelectorAll('.fa-building, .fa-syringe, .fa-flask, .fa-book-open, .fa-motorcycle, .fa-microscope, .fa-check-double, .fa-money-bill-1, .fa-box-archive');
+
+underlineElements.forEach((underlineTag, index) => {
+    underlineTag.addEventListener('mouseover', () => {
+        if (elements[index]) {
+            elements[index].style.color = '#9C3030';
+        }
+    });
+
+    underlineTag.addEventListener('mouseout', () => {
+        if (elements[index]) {
+            elements[index].style.color = '';
+        }
+    });
+});
