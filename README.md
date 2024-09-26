7c6e9e2f893abe019fcea2f3c5bf9847c0befa6b
Temi Michael <olasehindemichael285@gmail.com>
2024-09-26 10:26:28 +0100

    Code Completion

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ This is the Mock up code of the website task assigned on the 22nd of September 2
 
 Project was created using multiple display system for each required layout.
 
+Javascript added for responsive designs and elements placing into HTML File
+
 [Project Website]("https://e-lab-netwalkers.vercel.app/")
--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// NAMING VARIABLES
+
 let profileName = document.querySelector(".name");
 let labName = document.querySelector(".labName");
 const underlineElements = document.querySelectorAll('.underline');
 
+// PLACING NAMES TO TEXTS
 profileName.textContent = 'Isaac Odinaka';
 labName.textContent = profileName.textContent + "'s" + " Laboratory";
+
+
+// STYLING THE ICONS WHEN HOVERED OVER BY ITERATING OVER VARIOUS CLASS NAMES
 
 let elements = document.querySelectorAll('.fa-building, .fa-syringe, .fa-flask, .fa-book-open, .fa-motorcycle, .fa-microscope, .fa-check-double, .fa-money-bill-1, .fa-box-archive');
 
